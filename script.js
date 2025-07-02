7e20c61c457f2f970c2ff4efb405ee7259da3de4
Linda <lindasiegel135@gmail.com>
2025-07-02 19:54:18 +0200

    Mc quiz logic 2

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 function characterRandomizer(group) {
     return group[Math.floor(Math.random() * group.length)]
 }
+const answerButtons = ["Answer1", "Answer2", "Answer3", "Answer4" ];
+const correctButton = answerButtons[Math.floor(Math.random() * answerButtons.length)]
 let vowelAnswer = characterRandomizer(HiraganaVowels)
 
+
 document.getElementById("VowelCharacter").textContent = vowelAnswer.char
-document.getElementById("CorrectAnswer").textContent = vowelAnswer.romaji
-document.getElementById("WrongAnswer1").textContent = characterRandomizer(HiraganaVowels).romaji
-document.getElementById("WrongAnswer2").textContent = characterRandomizer(HiraganaVowels).romaji
-document.getElementById("WrongAnswer3").textContent = characterRandomizer(HiraganaVowels).romaji
+
+answerButtons.forEach(buttonId => {
+    if (buttonId === correctButton){
+document.getElementById(correctButton).textContent = vowelAnswer.romaji
+    } else{
+        let wrongAnswer;
+        do {
+            wrongAnswer = characterRandomizer(HiraganaVowels).romaji
+        }
+     while (wrongAnswer === vowelAnswer.romaji)
+        document.getElementById(buttonId).textContent = wrongAnswer
+}})
+
 
