87d2ccca0ac4d3b0f682ad8f6e61af20a000085e
Linda <lindasiegel135@gmail.com>
2025-07-02 10:47:17 +0200

    Mc layout

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ function characterRandomizer(group) {
 let vowelAnswer = characterRandomizer(HiraganaVowels)
 
 document.getElementById("VowelCharacter").textContent = vowelAnswer.char
-document.getElementById("Test").textContent = vowelAnswer.romaji
+document.getElementById("CorrectAnswer").textContent = vowelAnswer.romaji
+document.getElementById("WrongAnswer1").textContent = characterRandomizer(HiraganaVowels).romaji
+document.getElementById("WrongAnswer2").textContent = characterRandomizer(HiraganaVowels).romaji
+document.getElementById("WrongAnswer3").textContent = characterRandomizer(HiraganaVowels).romaji
 
