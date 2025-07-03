a531b53d89ae79dd93f8775d904e2210919ae59a
Linda <lindasiegel135@gmail.com>
2025-07-03 12:46:53 +0200

    MC answers bug fix

--- script.js
+++ script.js
@@ -2,23 +2,26 @@
 function characterRandomizer(group) {
     return group[Math.floor(Math.random() * group.length)]
 }
-const answerButtons = ["Answer1", "Answer2", "Answer3", "Answer4" ];
-const correctButton = answerButtons[Math.floor(Math.random() * answerButtons.length)]
+const answerButtons = ["Answer1", "Answer2", "Answer3", "Answer4"]; //all answer buttons to select from
+const correctButton = answerButtons[Math.floor(Math.random() * answerButtons.length)] //selects a random button to display the right answer
 let vowelAnswer = characterRandomizer(HiraganaVowels)
 
 
-document.getElementById("VowelCharacter").textContent = vowelAnswer.char
+document.getElementById("VowelCharacter").textContent = vowelAnswer.char //displays a random character of the chosen group
+const displayedAnswers = [vowelAnswer.romaji]
 
-answerButtons.forEach(buttonId => {
-    if (buttonId === correctButton){
-document.getElementById(correctButton).textContent = vowelAnswer.romaji
-    } else{
+answerButtons.forEach(buttonId => { //goes through all buttons and gives them an answer
+    if (buttonId === correctButton) {
+        document.getElementById(correctButton).textContent = vowelAnswer.romaji
+    } else {
         let wrongAnswer;
         do {
             wrongAnswer = characterRandomizer(HiraganaVowels).romaji
         }
-     while (wrongAnswer === vowelAnswer.romaji)
+        while (displayedAnswers.includes(wrongAnswer)); //lets do run as long as the answer is already displayed
         document.getElementById(buttonId).textContent = wrongAnswer
-}})
+        displayedAnswers.push(wrongAnswer) //stores already displayed answers
+    }
+});
 
 
